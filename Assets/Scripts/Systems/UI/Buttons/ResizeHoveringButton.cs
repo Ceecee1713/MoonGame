c1b90dce4b713d45bba026351be7a18ac71eb19c
Ceecee1713 <156458571+Ceecee1713@users.noreply.github.com>
2026-09-03 15:03:40 -0600

    Resize Button Script
    Made button script to create a resizing of the game object temporarily when the game object is hovered over. This is for UI buttons. Edited different UI prefabs to have this script attached to certain buttons

---
+++ Assets/Scripts/Systems/UI/Buttons/ResizeHoveringButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ResizeHoveringButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] 
+    private float hoverSizeScale = 1.1f;
+
+    private Tween tween;
+
+    private const float NORMAL_SIZE_SCALE = 1f;
+    private const float TWEEN_DURATION = 0.15f;
+
+    public void OnPointerEnter(PointerEventData eventData) //Scale up by "hoverSizeScale" when cursor is hovering over this game object
+    {
+        tween?.Kill();
+        tween = transform.DOScale(hoverSizeScale, TWEEN_DURATION).SetEase(Ease.OutQuad);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) //Return to normal scale size when cursor is not hovering over this game object
+    {
+        tween?.Kill();
+        tween = transform.DOScale(NORMAL_SIZE_SCALE, TWEEN_DURATION).SetEase(Ease.OutQuad);
+    }
+}
