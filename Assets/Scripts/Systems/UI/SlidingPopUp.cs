a33106363206bcc9b1bee02604761ae0828fd47b
Ceecee1713 <156458571+Ceecee1713@users.noreply.github.com>
2026-09-04 12:42:25 -0600

    Animating Dialogue Text Box - Scripts, Prefabs
    Made a new script to slide a UI pop up game object. This is for the dialogue text boxes to make them look more animated. Edited Moon Puzzle Warning UI and Main UI - Dialogue UI prefabs to include this script and change the position of the dialogue text box. Edited Chest prefab to have the resize hovering button script on its UI. Edited Game scene. Edited Main Player UI to move the inventory bar slightly

---
+++ Assets/Scripts/Systems/UI/SlidingPopUp.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SlidingPopUp : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform popUpRectTransform;
+
+    [SerializeField] 
+    private float distanceToMove = 50.0f;
+
+    [SerializeField] 
+    private float duration = 0.5f;
+
+    private Vector2 _originalPosition;
+    private Vector2 _upwardTargetPosition;
+
+    private bool _hasBeenDisabledOnStart = false;
+    private bool _isRaised = false; 
+
+    private Tween tween;
+
+    void Awake()
+    {
+        _originalPosition = new Vector2 (popUpRectTransform.anchoredPosition.x, popUpRectTransform.anchoredPosition.y);
+        _upwardTargetPosition = popUpRectTransform.anchoredPosition + new Vector2(0f, distanceToMove);
+    }
+
+    void OnEnable()
+    {
+        if(_hasBeenDisabledOnStart == true)
+        {
+            tween?.Kill();
+            tween = popUpRectTransform.DOAnchorPos(_upwardTargetPosition, duration).SetEase(Ease.OutQuad).SetLink(gameObject, LinkBehaviour.KillOnDestroy);;
+            _isRaised = true;
+        }
+    }
+
+    void OnDisable()
+    {
+        _hasBeenDisabledOnStart = true;
+        
+        if(_isRaised == true)
+        {
+            tween?.Kill();
+            tween = popUpRectTransform.DOAnchorPos(_originalPosition, duration).SetEase(Ease.OutQuad).SetLink(gameObject, LinkBehaviour.KillOnDestroy);;
+            _isRaised = false;
+        }
+    }
+}
